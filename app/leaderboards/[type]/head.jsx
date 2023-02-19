4263ac8e692bd3ad2265e8a79e74f41b4d24dd0d
Blackoutburst <blackoutburst@hotmail.com>
2023-02-19 23:06:41 +0100

    Fixed page descruption

--- app/leaderboards/[type]/head.jsx
+++ app/leaderboards/[type]/head.jsx
@@ -3,7 +3,7 @@ export default function Head({params}) {
     <>
       <title>{`Workshop - ${params?.type}`}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Check out the Hole in the Wall leaderboards!" />
+      <meta name="description" content={`Check out the ${params?.type} leaderboards!`} />
       <link rel="icon" href="/images/favicon.png" />
     </>
   )
