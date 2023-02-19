fb2d460722cc1fdcafd0d88f2039d8fa64e9ed25
Blackoutburst <blackoutburst@hotmail.com>
2023-02-19 23:05:56 +0100

    Fixed page name

--- app/leaderboards/[type]/head.jsx
+++ app/leaderboards/[type]/head.jsx
@@ -1,7 +1,7 @@
-export default function Head() {
+export default function Head({params}) {
   return (
     <>
-      <title>HitW - Leaderboards</title>
+      <title>{`Workshop - ${params?.type}`}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Check out the Hole in the Wall leaderboards!" />
       <link rel="icon" href="/images/favicon.png" />
